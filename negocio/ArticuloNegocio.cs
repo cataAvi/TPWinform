937ca0d221a17d15b9de02a03e0e2b4d682b03fc
CataAvi <avilarotelcatlina@gmail.com>
2024-09-13 16:57:56 -0300

    Se modifica la clase lista de artículos utilizando la clase de AccesoDatos, también se agrega el método listar para las Categorias

--- negocio/ArticuloNegocio.cs
+++ negocio/ArticuloNegocio.cs
@@ -12,51 +12,46 @@ namespace negocio
     {
         public List<Articulo> listar()
         {
-            List<Articulo> lista = new List<Articulo>();
-            SqlConnection conexion = new SqlConnection();
-            SqlCommand comando = new SqlCommand();
-            SqlDataReader lector;
 
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
             try
             {
+                datos.setearConsulta("select A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion as Marca, A.IdMarca, C.Descripcion as Categoria, A.IdCategoria, A.Precio, I.ImagenUrl from ARTICULOS A, IMAGENES I, MARCAS M, CATEGORIAS C where A.Id = I.IdArticulo and A.IdMarca = M.Id and A.IdCategoria = C.Id");
+                datos.ejecutarLectura();
 
-                conexion.ConnectionString = "server=DESKTOP-758I0CH\\ZOOLOGIC; database=CATALOGO_P3_DB; integrated security=true";
-                comando.CommandType = System.Data.CommandType.Text;
-                comando.CommandText = "select A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion as Marca, A.IdMarca, C.Descripcion as Categoria, A.IdCategoria, A.Precio, I.ImagenUrl from ARTICULOS A, IMAGENES I, MARCAS M, CATEGORIAS C where A.Id = I.IdArticulo and A.IdMarca = M.Id and A.IdCategoria = C.Id";
-                comando.Connection = conexion;
-
-                conexion.Open();
-                lector = comando.ExecuteReader();
-
-                while(lector.Read())
+                while (datos.Lector.Read())
                 {
                     Articulo aux = new Articulo();
-                    aux.Codigo = (string)lector["Codigo"];
-                    aux.Nombre = (string)lector["Nombre"];
-                    aux.Descripcion = (string)lector["Descripcion"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
                     //aux.Marca = (int)lector["IdMarca"];
-                    aux.Precio = (decimal)lector["Precio"];
-                    aux.Imagen = (string)lector["ImagenUrl"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+                    aux.Imagen = (string)datos.Lector["ImagenUrl"];
                     aux.Categoria = new Categoria();
-                    aux.Categoria.Codigo = (int)lector["IdCategoria"];
-                    aux.Categoria.Nombre = (string)lector["Categoria"];
+                    aux.Categoria.Codigo = (int)datos.Lector["IdCategoria"];
+                    aux.Categoria.Nombre = (string)datos.Lector["Categoria"];
                     aux.Marca = new Marca();
-                    aux.Marca.Codigo = (int)lector["IdMarca"];
-                    aux.Marca.Nombre = (string)lector["Marca"];
-
+                    aux.Marca.Codigo = (int)datos.Lector["IdMarca"];
+                    aux.Marca.Nombre = (string)datos.Lector["Marca"];
 
                     lista.Add(aux);
 
                 }
 
-                conexion.Close();
-                return lista; 
+                return lista;
             }
             catch (Exception ex)
             {
-                throw;
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
             }
 
         }
+
     }
 }
