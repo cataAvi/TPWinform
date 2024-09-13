dfa8518f5eea6b526ddab3155dc31f12d6271677
CataAvi <avilarotelcatlina@gmail.com>
2024-09-13 00:52:07 -0300

    Primera subida con listado de artículos teniendo en cuanta las clases de Categoria y Marca, también ver las imagenes de los productos (unica por el momento)

---
+++ TPWinforms/ArticuloNegocio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TPWinforms
+{
+    class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+
+                conexion.ConnectionString = "server=DESKTOP-758I0CH\\ZOOLOGIC; database=CATALOGO_P3_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion as Marca, A.IdMarca, C.Descripcion as Categoria, A.IdCategoria, A.Precio, I.ImagenUrl from ARTICULOS A, IMAGENES I, MARCAS M, CATEGORIAS C where A.Id = I.IdArticulo and A.IdMarca = M.Id and A.IdCategoria = C.Id";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while(lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Codigo = (string)lector["Codigo"];
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+                    //aux.Marca = (int)lector["IdMarca"];
+                    aux.Precio = (decimal)lector["Precio"];
+                    aux.Imagen = (string)lector["ImagenUrl"];
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Codigo = (int)lector["IdCategoria"];
+                    aux.Categoria.Nombre = (string)lector["Categoria"];
+                    aux.Marca = new Marca();
+                    aux.Marca.Codigo = (int)lector["IdMarca"];
+                    aux.Marca.Nombre = (string)lector["Marca"];
+
+
+                    lista.Add(aux);
+
+                }
+
+                conexion.Close();
+                return lista; 
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+}
---
+++ TPWinforms/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPWinforms
+{
+    public partial class Form1 : Form
+    {
+        private List<Articulo> listaArticulo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            listaArticulo = negocio.listar();
+            dgvArticulos.DataSource = listaArticulo;
+            dgvArticulos.Columns["Imagen"].Visible = false;
+            cargarImagen(listaArticulo[0].Imagen);
+
+        }
+
+        private void dgvArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+            Articulo seleccionado = (Articulo)dgvArticulos.CurrentRow.DataBoundItem;
+            cargarImagen(seleccionado.Imagen);
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            
+            try
+            {
+               pbxArticulo.Load(imagen);
+            }
+            catch(Exception ex)
+            {
+                pbxArticulo.Load("https://designshack.net/wp-content/uploads/placeholder-image.png");
+            }
+        }
+    }
+}
